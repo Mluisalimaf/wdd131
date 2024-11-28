3bca4dbc6599940fe9def5a0ef78f53cc84d5d40
maria luisa <limamarialuisa06@gmail.com>
2024-11-28 17:24:59 -0300

    changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,15 @@
 
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = "Last modified: " + lastModified;
+
+
+
+
+const hamButton = document.querySelector('.menu');
+const navigation = document.querySelector('.menu');
+
+hamButton.addEventListener('click', () => {
+	navigation.classlist.toggle('open');
+	hamButton.classList.toggle('open');
+    menu.classList.toggle("active");
+});
