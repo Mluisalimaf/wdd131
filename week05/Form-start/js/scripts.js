622b1dd838196fe004fec7597ecf5a742e794900
maria luisa <limamarialuisa06@gmail.com>
2024-12-18 19:10:28 -0300

    new files

---
+++ week05/Form-start/js/scripts.js
@@ -0,0 +1,49 @@
+let chaptersAray = getChapterList() || [];
+
+const input = document.querySelector('#favchapter');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+chaptersArray.foreach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChaptersList();
+        input.value = '';
+        input.focus();
+    }
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deletebutton = document.createElement('button');
+    li.textContent = item;
+    deletebutton.textContent = '❌';
+    deletebutton.classList.add('delete');
+    li.append(deletebutton);
+    list.append(li);
+
+    deletebutton.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    })
+}
+
+function setChaptersList() {
+    localStorage.setItem('myFavBOMList', JSON.stringify(chaptersArray));
+}
+
+function getChapterList() {
+    return JSON.parse(localStorage.getItem('myFavBOMList'));
+}
+
+function deleteChapter(chapter) {
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChaptersList();
+}
